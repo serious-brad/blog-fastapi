6d14b5671568e539d636cdd40b40b283f672fea2
Kob Brad <thewlad1k@gmail.com>
2026-09-06 03:22:09 +0300

    Added templates, static

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-https://youtu.be/iukOehU5aF4?si=-JhLOfrqG5BalEh2&t=889
+# Course:
+https://youtu.be/iukOehU5aF4?si=UhymQIfXvN6mHxHk&t=2541
+
+# Course github repository
+https://github.com/CoreyMSchafer/FastAPI-Full-Course
+
+# App start
+uv run fastapi dev .\main.py
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 posts: list[dict] = [
     {
@@ -13,15 +19,16 @@ posts: list[dict] = [
     {
         "id": 2,
         "author": "Jane Doe",
-        "title": "Ptyrthon is grreat for web Development",
+        "title": "Python is grreat for web Development",
         "content": "Python is a great language fora web developnmebtnt, and FastAPI makes it even better",
         "date_posted": "April 21, 2025",
     }
 ]
 
-@app.get("/")
-def home() -> None:
-    return {"message": "Hello WORLD!"}
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def home(request: Request):
+    return templates.TemplateResponse(request, 'home.html', {'posts': posts, 'title': 'Home'})
 
 @app.get("/api/posts")
 def get_posts():
